4c33395db154fac5c3a9b2298245c5469e3c1a61
imran725544 <imranpanda40@gmail.com>
2023-05-18 12:15:31 +0500

    updated message final

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ export const Header = () => {
           <NavLink href="https://flowbite.com/" className="flex items-center">
             <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Cinemate
+              Cinea Mate
             </span>
           </NavLink>
 
